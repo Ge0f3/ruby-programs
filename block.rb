23adaa2a038597e9ac962939a21e78c96f03538a
Geoffrey <geo.geofe@gmail.com>
2017-03-12 11:38:44 -0500

    modified

--- block.rb
+++ block.rb
@@ -1,2 +1,8 @@
-def add(num1,num2)
-	
+puts "Enter a number"
+num = gets.chomp().to_i
+case num
+ when 0..20 then puts "The number is small"
+ when 21..50 then puts "The number is neither small nor large"
+ when 51..100 then puts "The number is large"
+ else puts "The number isnt in the list"
+end
